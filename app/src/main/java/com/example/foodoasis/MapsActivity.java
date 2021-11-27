d38f7fda01f045cdc35fa179639bf84060d41d89
Keyon Amirpanahi <keyon@keyoninfo.com>
2021-11-27 13:00:17 -0600

    Move camera to user location when nearCurrentButton clicked

--- app/src/main/java/com/example/foodoasis/MapsActivity.java
+++ app/src/main/java/com/example/foodoasis/MapsActivity.java
@@ -142,6 +142,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Log.d("Url",url);
 
             new PlaceTask().execute(url);
+            onLocationChanged(userLocation);
         });
 
         nearInputButton.setOnClickListener(view -> {
@@ -155,6 +156,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Log.d("Url", url);
 
             new PlaceTask().execute(url);
+            nearInputButton.setEnabled(false);
         });
 
         addToFavoritesButton.setOnClickListener(view -> {
